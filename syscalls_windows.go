dfc56a1f608122e46ab3085462c56fe2140dc186
Miaoren Xia <xiamr@ihep.ac.cn>
2019-04-11 21:26:07 +0800

    dhcp masq for tap0901 and auto select first unused card

--- syscalls_windows.go
+++ syscalls_windows.go
@@ -158,6 +158,47 @@ func compare_interfacename(tapkey, deviceid, interfaceName string) error {
 	return nil
 }
 
+func getdeviceids(componentID string) ([]string, error) {
+	k, err := registry.OpenKey(registry.LOCAL_MACHINE, tapDriverKey, registry.READ)
+	if err != nil {
+		return []string{}, fmt.Errorf("Failed to open the adapter registry, TAP driver may be not installed, %v", err)
+	}
+	defer k.Close()
+	// read all subkeys, it should not return an err here
+	keys, err := k.ReadSubKeyNames(-1)
+	if err != nil {
+		return []string{}, err
+	}
+	deviceids := make([]string,0)
+	// find the one matched ComponentId
+	for _, v := range keys {
+		key, err := registry.OpenKey(registry.LOCAL_MACHINE, tapDriverKey+"\\"+v, registry.READ)
+		if err != nil {
+			continue
+		}
+		val, _, err := key.GetStringValue("ComponentId")
+		if err != nil {
+			key.Close()
+			continue
+		}
+		if val == componentID {
+			val, _, err = key.GetStringValue("NetCfgInstanceId")
+			if err != nil {
+				key.Close()
+				continue
+			}
+			key.Close()
+			deviceids = append(deviceids,val)
+		}
+		key.Close()
+	}
+	if len(deviceids) == 0 {
+		return []string{}, fmt.Errorf("Failed to find the tap device in registry with specified ComponentId '%s', TAP driver may be not installed", componentID)
+	} else {
+		return deviceids, nil
+	}
+}
+
 // getdeviceid finds out a TAP device from registry, it *may* requires privileged right to prevent some weird issue.
 func getdeviceid(componentID string, interfaceName string) (deviceid string, err error) {
 	k, err := registry.OpenKey(registry.LOCAL_MACHINE, tapDriverKey, registry.READ)
@@ -227,6 +268,15 @@ func setTUN(fd syscall.Handle, network string) error {
 	if localIP.To4() == nil {
 		return fmt.Errorf("Provided network(%s) is not a valid IPv4 address", network)
 	}
+    code1  := make([]byte, 0, 16)
+	code1 = append(code1,localIP.To4()[:4]...)
+	code1 = append(code1, remoteNet.Mask[:4]...)
+	code1 = append(code1, remoteNet.IP.To4()[:4]...)
+	code1 = append(code1, 16, 14, 0, 0) // For 1 Hour
+//	code1 := []byte { 10, 0, 0, 2, 255, 255, 255, 0, 10, 0, 0, 1, 1, 1, 81, 128 }
+	if err := syscall.DeviceIoControl(fd, tap_win_ioctl_config_dhcp_masq, &code1[0], uint32(16), &rdbbuf[0], uint32(len(rdbbuf)), &bytesReturned, nil); err != nil {
+		return err
+	}
 	code2 := make([]byte, 0, 12)
 	code2 = append(code2, localIP.To4()[:4]...)
 	code2 = append(code2, remoteNet.IP.To4()[:4]...)
@@ -240,32 +290,118 @@ func setTUN(fd syscall.Handle, network string) error {
 	return nil
 }
 
+// // openDev find and open an interface.
+// func openDev(config Config) (ifce *Interface, err error) {
+// 	// find the device in registry.
+// 	deviceid, err := getdeviceid(config.PlatformSpecificParams.ComponentID, config.PlatformSpecificParams.InterfaceName)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	path := "\\\\.\\Global\\" + deviceid + ".tap"
+// 	pathp, err := syscall.UTF16PtrFromString(path)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	// type Handle uintptr
+// 	file, err := syscall.CreateFile(pathp, syscall.GENERIC_READ|syscall.GENERIC_WRITE, uint32(syscall.FILE_SHARE_READ|syscall.FILE_SHARE_WRITE), nil, syscall.OPEN_EXISTING, syscall.FILE_ATTRIBUTE_SYSTEM|syscall.FILE_FLAG_OVERLAPPED, 0)
+// 	// if err hanppens, close the interface.
+// 	defer func() {
+// 		if err != nil {
+// 			syscall.Close(file)
+// 		}
+// 		if err := recover(); err != nil {
+// 			syscall.Close(file)
+// 		}
+// 	}()
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	var bytesReturned uint32
+
+// 	// find the mac address of tap device, use this to find the name of interface
+// 	mac := make([]byte, 6)
+// 	err = syscall.DeviceIoControl(file, tap_win_ioctl_get_mac, &mac[0], uint32(len(mac)), &mac[0], uint32(len(mac)), &bytesReturned, nil)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	// fd := os.NewFile(uintptr(file), path)
+// 	ro, err := newOverlapped()
+// 	if err != nil {
+// 		return
+// 	}
+// 	wo, err := newOverlapped()
+// 	if err != nil {
+// 		return
+// 	}
+// 	fd := &wfile{fd: file, ro: ro, wo: wo}
+// 	ifce = &Interface{isTAP: (config.DeviceType == TAP), ReadWriteCloser: fd}
+
+// 	// bring up device.
+// 	if err := setStatus(file, true); err != nil {
+// 		return nil, err
+// 	}
+
+// 	//TUN
+// 	if config.DeviceType == TUN {
+// 		if err := setTUN(file, config.PlatformSpecificParams.Network); err != nil {
+// 			return nil, err
+// 		}
+// 	}
+
+// 	// find the name of tap interface(u need it to set the ip or other command)
+// 	ifces, err := net.Interfaces()
+// 	if err != nil {
+// 		return
+// 	}
+
+// 	for _, v := range ifces {
+// 		if bytes.Equal(v.HardwareAddr[:6], mac[:6]) {
+// 			ifce.name = v.Name
+// 			return
+// 		}
+// 	}
+
+// 	return nil, errIfceNameNotFound
+// }
+
+
 // openDev find and open an interface.
 func openDev(config Config) (ifce *Interface, err error) {
 	// find the device in registry.
-	deviceid, err := getdeviceid(config.PlatformSpecificParams.ComponentID, config.PlatformSpecificParams.InterfaceName)
+	deviceids, err := getdeviceids(config.PlatformSpecificParams.ComponentID)
 	if err != nil {
 		return nil, err
 	}
-	path := "\\\\.\\Global\\" + deviceid + ".tap"
-	pathp, err := syscall.UTF16PtrFromString(path)
-	if err != nil {
-		return nil, err
-	}
-	// type Handle uintptr
-	file, err := syscall.CreateFile(pathp, syscall.GENERIC_READ|syscall.GENERIC_WRITE, uint32(syscall.FILE_SHARE_READ|syscall.FILE_SHARE_WRITE), nil, syscall.OPEN_EXISTING, syscall.FILE_ATTRIBUTE_SYSTEM|syscall.FILE_FLAG_OVERLAPPED, 0)
-	// if err hanppens, close the interface.
-	defer func() {
+	var file syscall.Handle
+	for i := range deviceids {
+		deviceid := deviceids[i]
+		path := "\\\\.\\Global\\" + deviceid + ".tap"
+		var pathp *uint16
+		pathp, err = syscall.UTF16PtrFromString(path)
 		if err != nil {
-			syscall.Close(file)
+			return nil, err
 		}
-		if err := recover(); err != nil {
-			syscall.Close(file)
+		// type Handle uintptr
+		file, err = syscall.CreateFile(pathp, syscall.GENERIC_READ|syscall.GENERIC_WRITE, uint32(syscall.FILE_SHARE_READ|syscall.FILE_SHARE_WRITE), nil, syscall.OPEN_EXISTING, syscall.FILE_ATTRIBUTE_SYSTEM|syscall.FILE_FLAG_OVERLAPPED, 0)
+		// if err hanppens, close the interface.
+		defer func() {
+			if err != nil {
+				syscall.Close(file)
+			}
+			if err := recover(); err != nil {
+				syscall.Close(file)
+			}
+		}()
+		if err != nil {
+			continue
 		}
-	}()
+		break
+	}
 	if err != nil {
 		return nil, err
 	}
+
 	var bytesReturned uint32
 
 	// find the mac address of tap device, use this to find the name of interface
